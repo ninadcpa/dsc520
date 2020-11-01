9c72fb6c324ca0a3199a7b693521a3be0415402d
ninadcpa <ninad.patkhedkar@gmail.com>
2020-11-01 23:59:37 +0000

    Exercise16 and output

--- completed/assignment09/kmeans_test.R
+++ completed/assignment09/kmeans_test.R
@@ -4,6 +4,8 @@ library(caTools)
 library(knitr)
 library(pander)
 library(class)
+library(tidyverse)
+library(factoextra)
 
 setwd("/cloud/project/completed/assignment09")
 bi_df <- read.csv("binary-classifier-data.csv")
@@ -70,3 +72,55 @@ ggplot(data=k_accuracy_df, aes(x=k_vals, y=accuracy_vals, group=1)) +
 
 autoplot(kmeans(bi_df, 10), data = bi_df,
          label = TRUE, label.size = 3, frame = TRUE)
+
+######################## Ex 16 #################
+setwd("/cloud/project/")
+
+ex16_data <- read.csv("data/clustering-data.csv")
+head(ex16_data)
+ggplot(ex16_data, aes(x=x, y=y)) +
+  geom_point(shape=18, color="orange")
+
+kmean_output <- kmeans(ex16_data, 3)
+summary(kmean_output)
+for (i in 2:12) {
+  print(i)
+}
+autoplot(kmeans(ex16_data, 3), data = ex16_data, 
+         label = TRUE, label.size = 3, frame = TRUE)
+
+set.seed(123)
+for(i in 2:12){
+  set.seed(123)
+  ex16_data <- read.csv("data/clustering-data.csv")
+  ex16_data.cluster <- kmeans(ex16_data, i)
+  ex16_data$cluster <- as.factor(ex16_data.cluster$cluster)
+  p <- ggplot(data = ex16_data, 
+              aes(x = x, 
+                  y = y, 
+                  color = cluster)) + 
+    geom_point(size = 0.3) + 
+    geom_point(data = as.data.frame(ex16_data.cluster$centers), 
+               color = "black", 
+               shape = 20, 
+               size = 3) + 
+    ggtitle(paste("K-Means with k == ", i, sep ="")) +
+    theme_bw()
+  print(p)
+}
+fviz_nbclust(ex16_data, kmeans, method = "wss")
+# function to compute total within-cluster sum of square 
+wss <- function(k) {
+  kmeans(ex16_data, k, nstart = 10 )$tot.withinss
+}
+
+# Compute and plot wss for k = 1 to k = 15
+k.values <- 2:12
+
+# extract wss for 2-15 clusters
+wss_values <- map_dbl(k.values, wss)
+wss_values
+plot(k.values, wss_values,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
